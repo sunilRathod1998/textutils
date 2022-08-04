3fb1bce67863f169388ad13c3061e0a48b054ce4
sunilRathod1998 <102358889+sunilRathod1998@users.noreply.github.com>
2022-08-04 17:34:16 +0530

    Updates

---
+++ precache-manifest.c782cfaa6f9cde5d27302eb8b5731fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bf3365c856373e65823",
+    "url": "/textutils/static/css/main.f1149c15.chunk.css"
+  },
+  {
+    "revision": "6bf3365c856373e65823",
+    "url": "/textutils/static/js/main.6bf3365c.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "9e4f2e11a50b0c9fcd9ecad02423ec5c",
+    "url": "/textutils/index.html"
+  }
+];
